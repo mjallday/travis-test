2dbbec3d54c089f764c7fee1d44cdce217a87639
Marshall Jones <marshall@balancedpayments.com>
2014-06-16 13:35:06 -0700

    23

--- create-pypi.sh
+++ create-pypi.sh
@@ -1,1 +1,3 @@
-echo -e "[distutils]\\n\tpypi\n\tbalanced\n\n[balanced]\n\trepository: $PYPI_SERVER\n\tusername: $PYPI_USERNAME\n\tpassword: $PYPI_PASSWORD\n" > ~/.pypirc
+echo -e "[distutils]\n\tpypi\n\tbalanced\n\n[balanced]\n\trepository: $PYPI_SERVER\n\tusername: $PYPI_USERNAME\n\tpassword: $PYPI_PASSWORD\n" > ~/.pypirc
+echo -e "[easy_install]\nindex_url = https://$PYPI_USERNAME:$PYPI_PASSWORD@pypi.vandelay.io/balanced/prod/+simple/" > ~/.pydistutils.cfg
+
