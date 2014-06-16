4ccaa04c7fb1f84d031e97ed027f0d79c44b7efb
Marshall Jones <marshall@balancedpayments.com>
2014-06-16 14:53:30 -0700

    pip.config

--- create-pypi.sh
+++ create-pypi.sh
@@ -1,4 +1,4 @@
 mkdir -p ~/.pip
 echo -e "[distutils]\n\tpypi\n\tbalanced\n\n[balanced]\n\trepository: $PYPI_SERVER\n\tusername: $PYPI_USERNAME\n\tpassword: $PYPI_PASSWORD\n" > ~/.pypirc
 echo -e "[easy_install]\nindex_url = https://$PYPI_USERNAME:$PYPI_PASSWORD@pypi.vandelay.io/balanced/prod/+simple/" > ~/.pydistutils.cfg
-echo -e "[global]\nindex-url = https://$PYPI_USERNAME:$PYPI_PASSWORD@pypi.vandelay.io/balanced/prod/+simple/" > ~/.pip/pip.config
+echo -e "[global]\nextra-index-url = https://$PYPI_USERNAME:$PYPI_PASSWORD@pypi.vandelay.io/balanced/prod/+simple/" > ~/.pip/pip.config
